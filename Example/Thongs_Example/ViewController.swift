0ba5ebbcecc2842b647b28b2c4bb1c96716713c0
Tomi Koskinen <tomi.koskinen@reaktor.fi>
2015-10-17 19:40:03 +0300

    add text to example

--- Example/Thongs_Example/ViewController.swift
+++ Example/Thongs_Example/ViewController.swift
@@ -7,20 +7,30 @@
 //
 
 import UIKit
+import Thongs
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        view.backgroundColor = UIColor.blueColor()
+        let titleLabel = UILabel(frame: CGRect(x: 20, y: 44, width: view.bounds.size.width-2*20, height: 40))
         
-        let titleLabel = UILabel(frame: CGRect(x: 0, y: 44, width: 350, height: 40))
-        titleLabel.backgroundColor = UIColor.redColor()
-        titleLabel.text = "jee"
+        let textBox = UITextView(frame: CGRect(x: CGRectGetMinX(titleLabel.frame) , y: CGRectGetMaxY(titleLabel.frame) + 20, width: titleLabel.frame.size.width, height: view.bounds.size.height - CGRectGetMaxY(titleLabel.frame) - 2*20))
         
         view.addSubview(titleLabel)
+        view.addSubview(textBox)
+        
+        // Attributed string to both text boxes created with Thongs
+        let red = thongs_color(UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1))
+        let large = thongs_font(UIFont.systemFontOfSize(30))
+        let kerning = thongs_kerning(1.4)
+        titleLabel.attributedText = (red <*> large <*> kerning)(thongs_string("This thing right here"))
+        
+        let blue = thongs_color(UIColor(red: 46/255, green: 204/255, blue: 133/255, alpha: 1))
+        let smallFont = thongs_font(UIFont.systemFontOfSize(11))
+        let res = (red <*> large) ~~> "Is lettin all the ladies know" <+> (smallFont <*> blue) ~~> "What guys talk about"
+        textBox.attributedText = res
     }
 
     override func didReceiveMemoryWarning() {
