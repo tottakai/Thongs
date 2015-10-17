995c8ea64dbb6921214e8b33ffc4dcb2e4ef3706
Tomi Koskinen <tomi.koskinen@reaktor.fi>
2015-10-17 19:09:52 +0300

    add content to example

--- Example/Thongs_Example/ViewController.swift
+++ Example/Thongs_Example/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.blueColor()
+        
+        let titleLabel = UILabel(frame: CGRect(x: 0, y: 44, width: 350, height: 40))
+        titleLabel.backgroundColor = UIColor.redColor()
+        titleLabel.text = "jee"
+        
+        view.addSubview(titleLabel)
     }
 
     override func didReceiveMemoryWarning() {
