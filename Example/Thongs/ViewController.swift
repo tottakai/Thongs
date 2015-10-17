0749fcb747d9da8d795d1cd08aac856aeda68b50
Tomi Koskinen <tomi.koskinen@reaktor.fi>
2015-10-17 17:37:50 +0300

    add example

---
+++ Example/Thongs/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Thongies
+//
+//  Created by Tomi Koskinen on 10/17/2015.
+//  Copyright (c) 2015 Tomi Koskinen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Thongs_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thongs_Example
+//
+//  Created by Tomi Koskinen on 17/10/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
