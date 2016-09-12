8a0940e9dac7e9d624396aba7558821870d715ea
Tomi Koskinen <tomi.koskinen@reaktor.fi>
2016-09-12 14:45:43 +0300

    some passing tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -15,148 +15,151 @@ extension String {
 extension NSAttributedString {
     func attributesAt(_ loc: Int, _ len: Int) -> [AnyHashable: Any] {
         var range = NSMakeRange(loc, len)
-        return self.attributesAtIndex(loc, effectiveRange: &range)
+        return self.attributes(at: loc, effectiveRange: &range)
     }
 }
 
-typealias NSAttributedStringAttribute = [AnyHashable: Any]
 
-func containsAttribute(_ key: String, _ value: String) -> MatcherFunc<NSAttributedStringAttribute> {
-    return MatcherFunc { actualExpression, failureMessage in
-        failureMessage.postfixMessage = "contain attribute <[\(key) : \(value)]>"
-        do {
-            if let attributeStringAttribute = try actualExpression.evaluate(),
-               let attr = attributeStringAttribute[key] {
-                    let attributeDescription = attr.description
-                    return attributeDescription.rangeOfString(value) != nil
-            }
-            return false
-        } catch _ {
+func fontAttribute(_ description: String) -> (AnyHashable, Any) -> Bool {
+    return { (key: AnyHashable, value: Any) in
+        guard let attributeName = key.base as? String, attributeName == "NSFont" else {
             return false
         }
+        
+        guard let font = value as? UIFont else {
+            return false
+        }
+        
+        let parts = font.description.characters.split(separator: ";")
+        return parts.contains(where: { part in
+            return String(part).trimmingCharacters(in: CharacterSet.whitespaces) == description
+        })
+    }
+}
+
+func colorAttribute(_ description: String) -> (AnyHashable, Any) -> Bool {
+    return { (key: AnyHashable, value: Any) in
+        guard let attributeName = key.base as? String, attributeName == "NSColor" else {
+            return false
+        }
+        
+        guard let color = value as? UIColor else {
+            return false
+        }
+        
+        return color.description == description
     }
 }
 
 
 class Tests: XCTestCase {
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testAttributed_string_length_and_content_is_the_string() {
+        let example = "foo"
+        XCTAssert(thongs_string(example).length == example.length)
+        XCTAssert(thongs_string(example).string == example)
     }
-}
 
-
-/*
-class ThongsSpec: QuickSpec {
-    override func spec() {
-        describe("Thongs") {
-
-            it("attributed string length and content is the string") {
-                let example = "foo"
-                expect(thongs_string(example).length) == example.length
-                expect(thongs_string(example).string) == example
-            }
-            
-            it("large font string length and content matches string length") {
-                let example = "test string"
-                let sample = thongs_font(largeFont)(thongs_string(example))
-                
-                expect(sample.length) == example.length
-                expect(sample.string) == example
-
-                let attributesOfFirstWord = sample.attributesAt(0, example.length)
-                expect(attributesOfFirstWord.count) == 1
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-weight: bold"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-style: normal"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-size: 23.00pt"))
-            }
-            
-            it("create string formatter and apply it to string") {
-                let formatter = thongs_color(UIColor.redColor()) <*> thongs_font(largeFont)
-                let result = formatter(thongs_string("jee"))
-                
-                expect(result.length) == "jee".length
-                expect(result.string) == "jee"
-
-                let attributesOfFirstWord = result.attributesAt(0, result.length)
-                expect(attributesOfFirstWord.count) == 2
-                expect(attributesOfFirstWord).to(containsAttribute("NSColor", "UIDeviceRGBColorSpace 1 0 0 1"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-weight: bold"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-style: normal"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-size: 23.00pt"))
-            }
-
-            it("operator piping produced same string as function composition") {
-                let example = "test string"
-                let red = thongs_color(UIColor.redColor())
-                let large = thongs_font(largeFont)
-                let result = (red <*> large)(thongs_string(example))
-                
-                expect(result.length) == example.length
-                expect(result.string) == example
-            }
+    func testLarge_font_string_length_and_content_matches_string_length() {
+        let example = "test string"
+        let sample = thongs_font(largeFont)(thongs_string(example))
         
-            it("attributed string creation with operators created intended text") {
-                let example = "test string"
-                let red = thongs_color(UIColor.redColor())
-                let large = thongs_font(largeFont)
-                let result = (red <*> large) ~~> example
-                
-                expect(result.length) == example.length
-                expect(result.string) == example
+        XCTAssert(sample.length == example.length)
+        XCTAssert(sample.string == example)
+        
+        let attributesOfFirstWord = sample.attributesAt(0, example.length)
+        XCTAssert(attributesOfFirstWord.count == 1)
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-weight: bold")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+    }
 
-                let attributesOfFirstWord = result.attributesAt(0, example.length)
-                expect(attributesOfFirstWord.count) == 2
-                expect(attributesOfFirstWord).to(containsAttribute("NSColor", "UIDeviceRGBColorSpace 1 0 0 1"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-weight: bold"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-style: normal"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-size: 23.00pt"))
-            }
-            
-            it("string concatenation with function composition") {
-                let red = thongs_color(UIColor.redColor())
-                let blue = thongs_color(UIColor.blueColor())
-                let large = thongs_font(largeFont)
-                let small = thongs_font(smallFont)
-                let result = (red <*> large) ~~> "two" <+> (small <*> blue) ~~> "three"
-                
-                expect(result.length) == "twothree".length
-                expect(result.string) == "twothree"
-  
-                let attributesOfFirstWord = result.attributesAt(0, 3)
-                expect(attributesOfFirstWord.count) == 2
-                expect(attributesOfFirstWord).to(containsAttribute("NSColor", "UIDeviceRGBColorSpace 1 0 0 1"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-style: normal; font-size: 23.00pt"))
-
-                let attributesOfSecondWord = result.attributesAt(3, 5)
-                expect(attributesOfFirstWord.count) == 2
-                expect(attributesOfSecondWord).to(containsAttribute("NSColor", "UIDeviceRGBColorSpace 0 0 1 1"))
-                expect(attributesOfSecondWord).to(containsAttribute("NSFont", "font-style: normal; font-size: 11.00pt"))
-            }
-
-            it("combine multiple string to text") {
-                let formatter = thongs_color(UIColor.redColor()) <*> thongs_font(largeFont)
-                let formatter2 = thongs_color(UIColor.blueColor()) <*> thongs_font(smallFont)
-                let result = formatter ~~> "two" <+> thongs_string(" ") <+> formatter2 ~~> "three"
-                
-                expect(result.length) == "two three".length
-                expect(result.string) == "two three"
-
-                let attributesOfFirstWord = result.attributesAt(0, 3)
-                expect(attributesOfFirstWord.count) == 2
-                expect(attributesOfFirstWord).to(containsAttribute("NSColor", "UIDeviceRGBColorSpace 1 0 0 1"))
-                expect(attributesOfFirstWord).to(containsAttribute("NSFont", "font-style: normal; font-size: 23.00pt"))
-
-                let attributesOfSpace = result.attributesAt(3, 1)
-                expect(attributesOfSpace.count) == 0
-
-                let attributesOfSecondWord = result.attributesAt(4, 5)
-                expect(attributesOfFirstWord.count) == 2
-                expect(attributesOfSecondWord).to(containsAttribute("NSColor", "UIDeviceRGBColorSpace 0 0 1 1"))
-                expect(attributesOfSecondWord).to(containsAttribute("NSFont", "font-style: normal; font-size: 11.00pt"))
-            }
-        }
+    func testCreate_string_formatter_and_apply_it_to_string() {
+        let formatter = thongs_color(UIColor.red) <*> thongs_font(largeFont)
+        let result = formatter(thongs_string("jee"))
+        
+        XCTAssert(result.length == "jee".length)
+        XCTAssert(result.string == "jee")
+        
+        let attributesOfFirstWord = result.attributesAt(0, result.length)
+        XCTAssert(attributesOfFirstWord.count == 2)
+        XCTAssert(attributesOfFirstWord.contains(where: colorAttribute("UIExtendedSRGBColorSpace 1 0 0 1")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-weight: bold")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+    }
+    
+    func testOperator_piping_produced_same_string_as_function_composition() {
+        let example = "test string"
+        let red = thongs_color(UIColor.red)
+        let large = thongs_font(largeFont)
+        let result = (red <*> large)(thongs_string(example))
+        
+        XCTAssert(result.length == example.length)
+        XCTAssert(result.string == example)
+    }
+ 
+    func testAttributed_string_creation_with_operators_created_intended_text() {
+        let example = "test string"
+        let red = thongs_color(UIColor.red)
+        let large = thongs_font(largeFont)
+        let result = (red <*> large) ~~> example
+        
+        XCTAssert(result.length == example.length)
+        XCTAssert(result.string == example)
+        
+        let attributesOfFirstWord = result.attributesAt(0, example.length)
+        XCTAssert(attributesOfFirstWord.count == 2)
+        XCTAssert(attributesOfFirstWord.contains(where: colorAttribute("UIExtendedSRGBColorSpace 1 0 0 1")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-weight: bold")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+    }
+    
+    func testString_concatenation_with_function_composition() {
+        let red = thongs_color(UIColor.red)
+        let blue = thongs_color(UIColor.blue)
+        let large = thongs_font(largeFont)
+        let small = thongs_font(smallFont)
+        let result = (red <*> large) ~~> "two" <+> (small <*> blue) ~~> "three"
+        
+        XCTAssert(result.length == "twothree".length)
+        XCTAssert(result.string == "twothree")
+        
+        let attributesOfFirstWord = result.attributesAt(0, 3)
+        XCTAssert(attributesOfFirstWord.count == 2)
+        XCTAssert(attributesOfFirstWord.contains(where: colorAttribute("UIExtendedSRGBColorSpace 1 0 0 1")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+        
+        let attributesOfSecondWord = result.attributesAt(3, 5)
+        XCTAssert(attributesOfFirstWord.count == 2)
+        XCTAssert(attributesOfSecondWord.contains(where: colorAttribute("UIExtendedSRGBColorSpace 0 0 1 1")))
+        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute("font-style: normal")))
+        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute("font-size: 11.00pt")))
+    }
+    
+    func testCombine_multiple_string_to_text() {
+        let formatter = thongs_color(UIColor.red) <*> thongs_font(largeFont)
+        let formatter2 = thongs_color(UIColor.blue) <*> thongs_font(smallFont)
+        let result = formatter ~~> "two" <+> thongs_string(" ") <+> formatter2 ~~> "three"
+        
+        XCTAssert(result.length == "two three".length)
+        XCTAssert(result.string == "two three")
+        
+        let attributesOfFirstWord = result.attributesAt(0, 3)
+        XCTAssert(attributesOfFirstWord.count == 2)
+        XCTAssert(attributesOfFirstWord.contains(where: colorAttribute("UIExtendedSRGBColorSpace 1 0 0 1")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+        
+        let attributesOfSpace = result.attributesAt(3, 1)
+        XCTAssert(attributesOfSpace.count == 0)
+        
+        let attributesOfSecondWord = result.attributesAt(4, 5)
+        XCTAssert(attributesOfFirstWord.count == 2)
+        XCTAssert(attributesOfSecondWord.contains(where: colorAttribute("UIExtendedSRGBColorSpace 0 0 1 1")))
+        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute("font-style: normal")))
+        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute("font-size: 11.00pt")))
     }
 }
-*/
--- Thongs.podspec
+++ Thongs.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "Thongs"
-  s.version          = "0.9.1"
+  s.version          = "0.9.2"
   s.summary          = "Functional library for NSAttributedString creation in swift 2.1."
 
 # This description is used to generate tags and improve search results.
