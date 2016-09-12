9e2b2d66f6b09313674e0ccb8ac5ecca682aea40
Tomi Koskinen <tomi.koskinen@reaktor.fi>
2016-09-12 22:17:57 +0300

    improve tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,8 +3,8 @@ import XCTest
 import Thongs
 
 
-let largeFont = UIFont.boldSystemFont(ofSize: 23)
-let smallFont = UIFont.boldSystemFont(ofSize: 11)
+let largeFont = UIFont(name: "Avenir-Light", size: 23)!
+let smallFont = UIFont(name: "Courier", size: 11)!
 
 extension String {
     var length : Int {
@@ -20,7 +20,7 @@ extension NSAttributedString {
 }
 
 
-func fontAttribute(_ description: String) -> (AnyHashable, Any) -> Bool {
+func fontAttribute(_ referenceFont: UIFont) -> (AnyHashable, Any) -> Bool {
     return { (key: AnyHashable, value: Any) in
         guard let attributeName = key.base as? String, attributeName == "NSFont" else {
             return false
@@ -30,10 +30,7 @@ func fontAttribute(_ description: String) -> (AnyHashable, Any) -> Bool {
             return false
         }
         
-        let parts = font.description.characters.split(separator: ";")
-        return parts.contains(where: { part in
-            return String(part).trimmingCharacters(in: CharacterSet.whitespaces) == description
-        })
+        return font == referenceFont
     }
 }
 
@@ -69,9 +66,7 @@ class Tests: XCTestCase {
         
         let attributesOfFirstWord = sample.attributesAt(0, example.length)
         XCTAssert(attributesOfFirstWord.count == 1)
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-weight: bold")))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute(largeFont)))
     }
 
     func testCreate_string_formatter_and_apply_it_to_string() {
@@ -84,9 +79,7 @@ class Tests: XCTestCase {
         let attributesOfFirstWord = result.attributesAt(0, result.length)
         XCTAssert(attributesOfFirstWord.count == 2)
         XCTAssert(attributesOfFirstWord.contains(where: colorAttribute(UIColor.red)))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-weight: bold")))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute(largeFont)))
     }
     
     func testOperator_piping_produced_same_string_as_function_composition() {
@@ -111,9 +104,7 @@ class Tests: XCTestCase {
         let attributesOfFirstWord = result.attributesAt(0, example.length)
         XCTAssert(attributesOfFirstWord.count == 2)
         XCTAssert(attributesOfFirstWord.contains(where: colorAttribute(UIColor.red)))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-weight: bold")))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute(largeFont)))
     }
     
     func testString_concatenation_with_function_composition() {
@@ -129,14 +120,12 @@ class Tests: XCTestCase {
         let attributesOfFirstWord = result.attributesAt(0, 3)
         XCTAssert(attributesOfFirstWord.count == 2)
         XCTAssert(attributesOfFirstWord.contains(where: colorAttribute(UIColor.red)))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute(largeFont)))
         
         let attributesOfSecondWord = result.attributesAt(3, 5)
-        XCTAssert(attributesOfFirstWord.count == 2)
+        XCTAssert(attributesOfSecondWord.count == 2)
         XCTAssert(attributesOfSecondWord.contains(where: colorAttribute(UIColor.blue)))
-        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute("font-style: normal")))
-        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute("font-size: 11.00pt")))
+        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute(smallFont)))
     }
     
     func testCombine_multiple_string_to_text() {
@@ -150,16 +139,14 @@ class Tests: XCTestCase {
         let attributesOfFirstWord = result.attributesAt(0, 3)
         XCTAssert(attributesOfFirstWord.count == 2)
         XCTAssert(attributesOfFirstWord.contains(where: colorAttribute(UIColor.red)))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-style: normal")))
-        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute("font-size: 23.00pt")))
+        XCTAssert(attributesOfFirstWord.contains(where: fontAttribute(largeFont)))
         
         let attributesOfSpace = result.attributesAt(3, 1)
         XCTAssert(attributesOfSpace.count == 0)
         
         let attributesOfSecondWord = result.attributesAt(4, 5)
-        XCTAssert(attributesOfFirstWord.count == 2)
+        XCTAssert(attributesOfSecondWord.count == 2)
         XCTAssert(attributesOfSecondWord.contains(where: colorAttribute(UIColor.blue)))
-        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute("font-style: normal")))
-        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute("font-size: 11.00pt")))
+        XCTAssert(attributesOfSecondWord.contains(where: fontAttribute(smallFont)))
     }
 }
